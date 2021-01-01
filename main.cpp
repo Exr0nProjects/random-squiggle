4d5fc514845b647f8c0722498ed8963abed20021
Exr0n <mail@exr0n.com>
2020-12-31 19:14:11 -0800

    snap, NW: init commit

---
+++ main.cpp
@@ -0,0 +1,155 @@
+// wiggle. uses Stefan Gustavson's SimplexNoise1234 (see below main() for details)
+
+#include <sys/ioctl.h>
+#include <unistd.h> // https://stackoverflow.com/a/23369919
+#include <cstdio>
+#define min(a, b) ((a)<(b)?(a):(b))
+#define max(a, b) ((a)<(b)?(b):(a))
+
+float noise(float c);
+
+int main()
+{
+    float cur = 0, sped = 0.01;
+    while (true)
+    {
+        float mn=0, mx=0;
+        float got = noise(cur += sped)*WIDTH/2;
+        for (int i=1; i<=got; ++i) putc(' ', stdout); printf("#\n");
+        mn = min(mn, got); mx = max(mx, got);
+        //printf("%10f %10f\n", mn, mx);
+    }
+}
+
+// SimplexNoise1234
+// Copyright © 2003-2011, Stefan Gustavson
+//
+// Contact: stegu@itn.liu.se
+//
+// This library is public domain software, released by the author
+// into the public domain in February 2011. You may do anything
+// you like with it. You may even remove all attributions,
+// but of course I'd appreciate it if you kept my name somewhere.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+
+/** \file
+		\brief Implements the SimplexNoise1234 class for producing Perlin simplex noise.
+		\author Stefan Gustavson (stegu@itn.liu.se)
+*/
+
+/*
+ * This implementation is "Simplex Noise" as presented by
+ * Ken Perlin at a relatively obscure and not often cited course
+ * session "Real-Time Shading" at Siggraph 2001 (before real
+ * time shading actually took on), under the title "hardware noise".
+ * The 3D function is numerically equivalent to his Java reference
+ * code available in the PDF course notes, although I re-implemented
+ * it from scratch to get more readable code. The 1D, 2D and 4D cases
+ * were implemented from scratch by me from Ken Perlin's text.
+ *
+ * This is a highly reusable class. It has no dependencies
+ * on any other file, apart from its own header file.
+ */
+
+
+#define FASTFLOOR(x) ( ((x)>0) ? ((int)x) : (((int)x)-1) )
+
+//---------------------------------------------------------------------
+// Static data
+
+/*
+ * Permutation table. This is just a random jumble of all numbers 0-255,
+ * repeated twice to avoid wrapping the index at 255 for each lookup.
+ * This needs to be exactly the same for all instances on all platforms,
+ * so it's easiest to just keep it as static explicit data.
+ * This also removes the need for any initialisation of this class.
+ *
+ * Note that making this an int[] instead of a char[] might make the
+ * code run faster on platforms with a high penalty for unaligned single
+ * byte addressing. Intel x86 is generally single-byte-friendly, but
+ * some other CPUs are faster with 4-aligned reads.
+ * However, a char[] is smaller, which avoids cache trashing, and that
+ * is probably the most important aspect on most architectures.
+ * This array is accessed a *lot* by the noise functions.
+ * A vector-valued noise over 3D accesses it 96 times, and a
+ * float-valued 4D noise 64 times. We want this to fit in the cache!
+ */
+unsigned char perm[512] = {151,160,137,91,90,15,
+  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180,
+  151,160,137,91,90,15,
+  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+};
+
+//---------------------------------------------------------------------
+
+/*
+ * Helper functions to compute gradients-dot-residualvectors (1D to 4D)
+ * Note that these generate gradients of more than unit length. To make
+ * a close match with the value range of classic Perlin noise, the final
+ * noise values need to be rescaled to fit nicely within [-1,1].
+ * (The simplex noise functions as such also have different scaling.)
+ * Note also that these noise functions are the most practical and useful
+ * signed version of Perlin noise. To return values according to the
+ * RenderMan specification from the SL noise() and pnoise() functions,
+ * the noise values need to be scaled and offset to [0,1], like this:
+ * float SLnoise = (SimplexNoise1234::noise(x,y,z) + 1.0) * 0.5;
+ */
+
+float grad( int hash, float x ) {
+    int h = hash & 15;
+    float grad = 1.0f + (h & 7);   // Gradient value 1.0, 2.0, ..., 8.0
+    if (h&8) grad = -grad;         // Set a random sign for the gradient
+    return ( grad * x );           // Multiply the gradient with the distance
+}
+
+// 1D simplex noise
+float noise(float x) {
+
+  int i0 = FASTFLOOR(x);
+  int i1 = i0 + 1;
+  float x0 = x - i0;
+  float x1 = x0 - 1.0f;
+
+  float n0, n1;
+
+  float t0 = 1.0f - x0*x0;
+//  if(t0 < 0.0f) t0 = 0.0f;
+  t0 *= t0;
+  n0 = t0 * t0 * grad(perm[i0 & 0xff], x0);
+
+  float t1 = 1.0f - x1*x1;
+//  if(t1 < 0.0f) t1 = 0.0f;
+  t1 *= t1;
+  n1 = t1 * t1 * grad(perm[i1 & 0xff], x1);
+  // The maximum value of this noise is 8*(3/4)^4 = 2.53125
+  // A factor of 0.395 would scale to fit exactly within [-1,1], but
+  // we want to match PRMan's 1D noise, so we scale it down some more.
+  return 0.395f * (n0 + n1);
+}
+
